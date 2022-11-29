7b10164c2f0d2bd31bea8b07ac3ec2a622139f82
Mr-robot <hackingtest017@gmail.com>
2022-11-29 12:58:55 +0300

    Python - if else loops and functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-last_num = int(repr(number)[-1])
-
-if number > 0:
-    if last_num > 5:
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_num))
-    elif last_num == 0:
-        print("Last digit of {:d} is {:d} and is 0".format(number, last_num))
-    elif last_num < 6:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_num))
+str1 = " and is greater than 5"
+str2 = " and is 0"
+str3 = " and is less than 6 and not 0"
+if number < 0:
+    last = number % -10
 else:
-    if last_num == 0:
-        print("Last digit of {:d} is {:d} and is 0".format(number, last_num * -1))
-    elif last_num < 5:
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_num * -1))
-    elif last_num > 6:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_num * -1))
+    last = number % 10
+if last > 5:
+    print("Last digit of {} is {}".format(number, last) + str1)
+elif last == 0:
+    print("Last digit of {} is {}".format(number, last) + str2)
+else:
+    print("Last digit of {} is {}".format(number, last) + str3)
