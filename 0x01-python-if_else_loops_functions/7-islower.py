306aba5b70ec00cd54e0acd8af44121466dffafd
Mr-robot <hackingtest017@gmail.com>
2022-11-29 15:55:06 +0300

    Python - if else loops and functions

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# a(97) z(122)
+def islower(c):
+    dicemal_value = ord(c)
+    if dicemal_value >= 97 and dicemal_value <= 122:
+        return True
+    else:
+        return False
