86f974f6d35d00ef1314e842c1dfc78b89dc05c4
Mr-robot <hackingtest017@gmail.com>
2022-11-29 16:31:23 +0300

    Python - if else loops and functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # sample best
 def uppercase(str):
-	for i in range(len(str)):
-		temp = ord(str[i])
-		if temp >= 97 and temp <= 122:
-			temp -= 32
-		print("{}".format(chr(char)), end="")
+    for i in range(len(str)):
+        temp = ord(str[i])
+
+        if temp >= 97 and temp <= 122:
+            temp -= 32
+        print("{}".format(chr(temp)), end="")
+    print()
