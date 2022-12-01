a8a4b12e3d6b467fa3f9c6cff051a39058484b52
Mr-robot <hackingtest017@gmail.com>
2022-12-01 23:52:22 +0300

    Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 import sys
 
 if __name__ == '__main__':
-    argument_length = len(sys.argv)
+    length = len(sys.argv)
 
     sum = 0
-    for i in range(1, argument_length):
+    for i in range(1, length):
         sum += int(sys.argv[i])
 
     print("{:d}".format(sum))
