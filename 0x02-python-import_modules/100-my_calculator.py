0b24e114f3af961766d129def9d7a41b4348086f
Mr-robot <hackingtest017@gmail.com>
2022-12-02 00:45:47 +0300

    Python - import & modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == '__main__':
+    warning = "Usage: ./100-my_calculator.py <a> <operator> <b>"
+    warning2 = "Unknown operator. Available operators: +, -, * and /"
+    length = len(argv)
+
+    if length <= 3:
+        print("{}".format(warning))
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        operator = argv[2]
+
+        if operator == '+':
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        elif operator == '-':
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+        elif operator == '*':
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        elif operator == '/':
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+        else:
+            print("{}\n{}".format(warning2, 1))
