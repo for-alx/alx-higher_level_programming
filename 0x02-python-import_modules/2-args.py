51e814be8f5eda0096952bddfab8b4a1dd7573ff
Mr-robot <hackingtest017@gmail.com>
2022-12-01 23:48:19 +0300

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,8 @@ import sys
 if __name__ == '__main__':
     length = len(sys.argv)
 
-
     if length - 1 == 0:
         print("{} arguments.".format(length - 1))
-
     else:
         for i in range(1, length):
             if i == 1:
