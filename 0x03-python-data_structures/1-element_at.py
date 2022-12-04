fd21637d1100251b175dab421de0c89d0c5a0a38
Mr-robot <hackingtest017@gmail.com>
2022-12-04 12:33:10 +0300

    thon - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx):
-    length = len(my_list)
+if __name__ == '__main__':
 
-    if idx > length:
-        return None
-    elif idx < 0:
-        return None
-    else:
-        return my_list[idx]
+    def element_at(my_list, idx):
+        length = len(my_list)
+
+        if idx > length:
+            return None
+        elif idx < 0:
+            return None
+        else:
+            return my_list[idx]
