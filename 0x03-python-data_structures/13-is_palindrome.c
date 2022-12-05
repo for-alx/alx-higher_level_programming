2edc4c564d09eba98ccceea7965e40998e48cf61
Mr-robot <hackingtest017@gmail.com>
2022-12-05 10:45:25 +0300

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,31 @@
 #include "lists.h"
 
 /**
- * pal - recursive helper function to determine if llst is palindrom
- * @start: start position of list
- * @end: end position of list
- * Return: 1 if list is a palindrome, 0 otherwise
- */
-
-int pal(listint_t **start, listint_t *end)
-{
-	if (end == NULL)
-		return (1);
-
-	if (pal(start, end->next) == 1 && (*start)->n == end->n)
-	{
-		*start = (*start)->next;
-		return (1);
-	}
-
-	return (0);
-}
-
-/**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: list to check
- * Return: 1 if list is a palindrome, 0 otherwise
+ * is_palindrome - function to call check_pal to see if list is palindrome
+ * @head: ptr to the beginning of the list
+ * Return: 0 if not palindrome else 1
  */
 int is_palindrome(listint_t **head)
 {
-	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
+	return (check_pal(head, *head));
+}
 
-	return (pal(head, *head));
+/**
+ * check_pal - function to check if the list is palindrome
+ * @head: ptr to the beginning of the list
+ * @last: ptr to the end of the list
+ * Return: 0 if not palindrom else 1
+ */
+int check_pal(listint_t **head, listint_t *last)
+{
+	if (last == NULL)
+		return (1);
+	if (check_pal(head, last->next) && (*head)->n == last->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
