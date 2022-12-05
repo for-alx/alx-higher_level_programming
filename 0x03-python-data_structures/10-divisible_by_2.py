adb8ae72da5c59c4b54f117312a4078c021ea6d6
Mr-robot <hackingtest017@gmail.com>
2022-12-05 10:10:02 +0300

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
-#!/usr/binpython3
+#!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    temp = []
+    new_list = []
 
-    for i in my_list:
-        if i % 2 == 0:
-            temp.append(True)
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(True)
         else:
-            temp.append(False)
-    return temp
+            new_list.append(False)
+    return new_list
