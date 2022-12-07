fd2b71cd0dc2b6f1db069e09a3e9bf5c1cc2e7e3
Mr-robot <hackingtest017@gmail.com>
2022-12-07 21:16:26 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_dir =  sorted(a_dictionary)
+    sorted_dir = sorted(a_dictionary)
 
     for k, v in sorted(a_dictionary.items()):
-        print(k,end=": ")
+        print(k, end=": ")
         print(v)
