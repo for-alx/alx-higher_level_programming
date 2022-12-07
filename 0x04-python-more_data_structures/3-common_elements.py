69666f5528a89f495644a7a7ca87f06d182d8aa9
Mr-robot <hackingtest017@gmail.com>
2022-12-07 15:25:47 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    result = {}
+    result = set()
 
     for item in set_1:
         if item in set_2:
-            result = item
+            result.add(item)
 
     return result
