d20c1adaaebb3aded0050ebd0e5433d7ce2f23a5
Mr-robot <hackingtest017@gmail.com>
2022-12-07 22:11:08 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
 
+    new = 0
+    key = None
+    for k, v in a_dictionary.items():
+        if new < v:
+            new = v
+            key = k
+    return key
