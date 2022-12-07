084b8ed4fe90bbd9f582995653caf583d1aa899e
Mr-robot <hackingtest017@gmail.com>
2022-12-07 14:11:26 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,5 @@
 #!/usr//bin/python3
 
 def square_matrix_simple(matrix=[]):
-    result = []
-    for row in matrix:
-        temp = []
-        k = 0
-        for item in row:
-            temp.append(item ** 2)
-            k += 1
-        result.append(temp)
-    return result
+    if matrix:
+        return [list(map(lambda j: j**2, i)) for i in matrix]
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-	result = {}
+    result = {}
 
-	for item in set_1:
-		if item in set_2:
-			result = item
+    for item in set_1:
+        if item in set_2:
+            result = item
 
-	return result
+    return result
