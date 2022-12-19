39687f6e6d623ef199f92ea284c74dca59bd2d22
Mr-robot <hackingtest017@gmail.com>
2022-12-19 21:04:16 +0300

    safe print 2

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+##### 0
 # safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 # my_list = [1, 2, 3, 4, 5]
@@ -12,20 +13,49 @@
 
 
 
+##### 1
+# safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+# value = 89
+# has_been_print = safe_print_integer(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
 
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+# value = -89
+# has_been_print = safe_print_integer(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
 
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+# value = "School"
+# has_been_print = safe_print_integer(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
 
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+
+##### 2
+#!/usr/bin/python3
+safe_print_list_integers = \
+    __import__('2-safe_print_list_integers').safe_print_list_integers
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
+
+
+
+
+
+
+
+
+
+
+
