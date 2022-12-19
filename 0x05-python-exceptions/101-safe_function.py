a887ec7f3986cd1f534ac153b48bb1719ede8721
Mr-robot <hackingtest017@gmail.com>
2022-12-19 22:55:10 +0300

    Python Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+def safe_function(fct, *args):
+    import sys
+    try:
+        return fct(*args)
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return None
