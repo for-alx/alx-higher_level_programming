1b2956a1653ac666d87d580b96778e6884e08145
Mr-robot <hackingtest017@gmail.com>
2022-12-20 19:53:24 +0300

    Python - Classes and Objects

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -2,32 +2,26 @@
 
 
 class Square:
-    """
-    creates square
-    """
+    '''creates square'''
 
     def __init__(self, size=0, position=(0, 0)):
-        """
+        '''
         initializes square
 
         Args:
             size: size of side of square
-        """
+        '''
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """
-        finds size
-        """
+        '''finds size'''
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        validates size is an integer that is greater than zero
-        """
+        '''validates size is an integer that is greater than zero'''
         if type(value) is not int:
             raise TypeError('size must be an integer')
         elif value < 0:
@@ -36,29 +30,27 @@ class Square:
             self.__size = value
 
     def area(self):
-        """
+        ''''
         finds area of square
 
         Returns:
             area of square
-        """
+        '''
         return self.__size ** 2
 
     @property
     def position(self):
-        """
-        finds position
-        """
+        '''finds position'''
         return self.__position
 
     @position.setter
     def position(self, value):
-        """
+        '''
         sets position
 
         Args:
             value: position of the square
-        """
+        '''
         if type(value) is not tuple or len(value) != 2 or \
            type(value[0]) is not int or value[0] < 0 or \
            type(value[1]) is not int or value[1] < 0:
@@ -67,9 +59,7 @@ class Square:
             self.__position = value
 
     def my_print(self):
-        """
-        prints square with character #
-        """
+        '''prints square with character'''
         if self.__size == 0:
             print("")
             return
@@ -79,9 +69,7 @@ class Square:
             print("{}{}".format(" " * self.__position[0], "#" * self.__size))
 
     def __str__(self):
-        """
-        returns string version of square
-        """
+        '''returns string version of square'''
         string = ""
         if self.__size == 0:
             return string
