bda61ddca4f2d54b1158a0b42eb70f0d89796c2b
Mr-robot <hackingtest017@gmail.com>
2022-12-20 19:47:11 +0300

    Python - Classes and Objects

--- 0x06-python-classes/100-singly_linked_list(1).py
+++ 0x06-python-classes/100-singly_linked_list(1).py
@@ -3,33 +3,29 @@
 
 
 class Node:
-    """
-    defines node
-    """
+    '''defines node'''
 
     def __init__(self, data, next_node=None):
-        """
+        '''
         initializes node
 
         Attributes:
             data: data stored in node
             next_node: address of next node in list
-        """
+        '''
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """
+        '''
         finds data
-        """
+        '''
         return self.__data
 
     @data.setter
     def data(self, value):
-        """
-        validates that data is an integer
-        """
+        '''validates that data is an integer'''
         if type(value) is not int:
             raise TypeError('data must be an integer')
         else:
@@ -37,16 +33,12 @@ class Node:
 
     @property
     def next_node(self):
-        """
-        finds next_node
-        """
+        '''finds next_node'''
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """
-        validates next_node as either None or a node
-        """
+        '''validates next_node as either None or a node'''
         if type(value) is not Node and value is not None:
             raise TypeError('next_node must be a Node object')
         else:
@@ -54,20 +46,14 @@ class Node:
 
 
 class SinglyLinkedList:
-    """
-    defines singly linked list class
-    """
+    '''defines singly linked list class'''
 
     def __init__(self):
-        """
-        initializes singly linked list
-        """
+        '''initializes singly linked list'''
         self.__head = None
 
     def __str__(self):
-        """
-        returns string version of list
-        """
+        '''returns string version of list'''
         string = ""
         if self.__head is None:
             return string
@@ -80,9 +66,7 @@ class SinglyLinkedList:
         return string
 
     def sorted_insert(self, value):
-        """
-        inserts a new node into correct position in list
-        """
+        '''inserts a new node into correct position in list'''
         if self.__head is None or value < self.__head.data:
             self.__head = Node(value, self.__head)
             return
