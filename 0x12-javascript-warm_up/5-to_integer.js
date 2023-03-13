1b533d50338f41870480f2faf7d11165600b22e4
Mr-robot <hackingtest017@gmail.com>
2023-03-13 23:17:20 +0300

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (args[2] === undefined) {
+  console.log('Not a number');
+} else if (parseInt(args[2])) {
+  console.log('My number: ', Math.floor(args[2]));
+} else {
+  console.log('Not a number');
+}
