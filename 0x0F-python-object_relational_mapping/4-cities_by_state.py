768ab2c7954200b3866a9a909dc4cd886435338a
Mr-robot <hackingtest017@gmail.com>
2023-03-18 12:43:14 +0300

    Task 4 fix join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-	import sys
-	import MySQLdb
-	args = sys.argv
-	# db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3])
-	# cur = db.cursor()
-
-	query = "SELECT * FROM table_name WHERE column_name = %s"
-
-	rows = cur.fetchall()
+    import sys
+    import MySQLdb
+    args = sys.argv
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities JOIN states \
+                ON cities.state_id = states.id ORDER BY cities.id;")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-
-
-
-
