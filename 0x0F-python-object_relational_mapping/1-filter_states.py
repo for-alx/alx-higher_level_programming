0fa7cca15a097c4e65dcb2c516821598fb5f4610
Mr-robot <hackingtest017@gmail.com>
2023-03-18 09:50:14 +0300

    Task 1 order fix

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Lists all states from database hbtn_0e_0_usa that start with 'N'
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    args = sys.argv
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
