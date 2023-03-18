fd2b4b8c41cc9e3588849855057ef46be5b17b27
Mr-robot <hackingtest017@gmail.com>
2023-03-18 14:33:19 +0300

    Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+    args = sys.argv
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+        JOIN states \
+        ON cities.state_id = states.id \
+        WHERE states.name=%s ORDER BY cities.id", (args[4], ))
+
+    rows = cur.fetchall()
+    print(", ".join(row[0] for row in rows))
