180823d2d2b049ffec4316262b44c75504cbe87d
Mr-robot <hackingtest017@gmail.com>
2023-03-30 21:11:19 +0300

    :)

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,8 @@
 """
     Peak finder
 """
+
+
 def find_peak(list_of_integers):
     """
     Finds a peak in a list of unsorted integers using binary search.
@@ -15,11 +17,11 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
     n = len(list_of_integers)
-    l, r = 0, n - 1
-    while l < r:
-        mid = (l + r) // 2
+    left, right = 0, n - 1
+    while left < right:
+        mid = (left + right) // 2
         if list_of_integers[mid] < list_of_integers[mid + 1]:
-            l = mid + 1
+            left = mid + 1
         else:
-            r = mid
-    return list_of_integers[l]
+            right = mid
+    return list_of_integers[left]
