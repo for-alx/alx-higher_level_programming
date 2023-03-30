aaffe2b53733931a7a4b033ccff727fcb1c056e0
Mr-robot <hackingtest017@gmail.com>
2023-03-30 19:41:26 +0300

    add -?I flag

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept.
-curl -s -X OPTIONS $* | grep "Allow:" | cut -d " " -f 2,3,4
+curl -sI -X OPTIONS $* | grep "Allow:" | cut -d " " -f 2,3,4
