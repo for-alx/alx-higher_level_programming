1cd2a38cb8c2c7ff8045f97b834fe36560788c4b
Mr-robot <hackingtest017@gmail.com>
2023-04-01 00:06:37 +0300

    ...

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,10 +12,13 @@ if __name__ == '__main__':
         data = str(sys.argv[1])
 
     value = {'q': data}
-    response = requests.post('http://0.0.0.0:5000/search_user', data=value)
-    # print(response.content.decode('utf-8'))
-    result = eval(response.content.decode('utf-8'))
-    if len(result) == 0:
-        print('No result')
-    else:
-        print('[{}] {}'.format(result['id'], result['name']))
+    try:
+        response = requests.post('http://0.0.0.0:5000/search_user', data=value)
+        # print(response.content.decode('utf-8'))
+        result = eval(response.content.decode('utf-8'))
+        if len(result) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(result['id'], result['name']))
+    except Exception:
+        print('Not a valid JSON')
