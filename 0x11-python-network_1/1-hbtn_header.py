3e2ce7c9e1f8707c84a4d06fb75236e63178d81c
Mr-robot <hackingtest017@gmail.com>
2023-03-31 22:00:18 +0300

    add sys.argv

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@ Response header value.
 if __name__ == '__main__':
     import sys
     from urllib.request import Request, urlopen
-    req = Request(str(sys.argv[0]))
+    req = Request(str(sys.argv[1]))
     with urlopen(req) as response:
         the_page = response.read()
         print(response.info().get('X-Request-Id'))
