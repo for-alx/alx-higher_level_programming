9932204876c7e71d8a2440c37f45a34d604377a4
Mr-robot <hackingtest017@gmail.com>
2023-03-31 21:58:43 +0300

    add sys.argv

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@ Response header value.
 if __name__ == '__main__':
     import sys
     from urllib.request import Request, urlopen
-    req = Request('https://alx-intranet.hbtn.io')
+    req = Request(str(sys.argv[0]))
     with urlopen(req) as response:
         the_page = response.read()
         print(response.info().get('X-Request-Id'))
