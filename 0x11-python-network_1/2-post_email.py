c63c3c24f1af36db892072ccaad89101f276a04f
Mr-robot <hackingtest017@gmail.com>
2023-03-31 22:38:55 +0300

    add utf-8 decode

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,16 +2,15 @@
 """
 POST an email.
 """
-
 if __name__ == '__main__':
     import sys
     from urllib.request import Request, urlopen
     import urllib.parse
 
     value = {'email': str(sys.argv[2])}
-    data = urllib.parse.urlencode(values)
+    data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
     req = Request(str(sys.argv[1]), data)
     with urlopen(req) as response:
         the_page = response.read()
-        print(str(the_page.decode('utf-8')))
+        print(the_page.decode('utf-8'))
