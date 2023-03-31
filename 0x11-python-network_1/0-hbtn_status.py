675569c7916a87f55814c1427f2c7aac8e9e3f92
Mr-robot <hackingtest017@gmail.com>
2023-03-31 21:22:51 +0300

    remove extra line

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with urlopen(req) as response:
         the_page = response.read()
         print('Body response:')
-        print('\t- type: ', type(the_page))
-        print('\t- content: ', the_page)
-        print('\t- utf8 content: ', the_page.decode('utf-8'))
+        print('\t- type: {}'.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
