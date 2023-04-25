b1605bfd7f3d05e97e8f5d756b31369449bb6dac
Mr-robot <hackingtest017@gmail.com>
2023-04-25 15:41:14 +0300

    hot fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,20 @@
 
 const request = require('request');
 
-// const url = process.argv[2].toString();
+const url = process.argv[2].toString();
+const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
 
-const url2 = 'https://swapi-api.alx-tools.com/api/people/18';
-
-request.get(url2, function (err, response, body) {
+request.get(url, function (err, response, body) {
   if (err) {
     console.error(err);
   }
-  console.log(JSON.parse(body).films.length);
+  const films = JSON.parse(body).results;
+  let count = 0;
+  let film = '';
+  for (film of films) {
+    if (film.characters.includes(actor)) {
+      count += 1;
+    }
+  }
+  console.log(count);
 });
