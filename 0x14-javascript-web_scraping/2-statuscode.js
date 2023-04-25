78a29e94d58bd544f392ae1ea3cc1c51d13c6281
Mr-robot <hackingtest017@gmail.com>
2023-04-25 13:15:36 +0300

    fix mixed tab error

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2].toString();
+
+request.get(url, function (err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('code: ', response.statusCode);
+});
