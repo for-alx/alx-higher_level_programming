560be206dda094d3b9c7d0fce1119c5aa239cc4c
Mr-robot <hackingtest017@gmail.com>
2023-04-25 22:54:47 +0300

    :)

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,19 +2,20 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request.get(url, function (err, response, body) {
-  if (err) {
-    return console.error(err);
+function helpRequest (actors, index) {
+  if (index === actors.length) {
+    return;
   }
-  const actors = JSON.parse(body).characters;
-  for (const actor of actors) {
-    request.get(actor, function (error, resp, person) {
-      if (err) {
-        return console.error(error);
-      }
-      console.log(JSON.parse(person).name);
-    });
-  }
+  request(actors[index], function (error, response, body) {
+    if (error) { console.log(error); }
+    console.log(JSON.parse(body).name);
+    helpRequest(actors, index + 1);
+  });
+}
+request(url, function (error, response, body) {
+  if (error) { console.error(error); }
+  const result = JSON.parse(body).characters;
+  helpRequest(result, 0);
 });
